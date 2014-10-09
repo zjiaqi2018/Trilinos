zjiaqi2018/Trilinos

commit 6e712e60682a16d6aa013f60010265f58632f279

MueLu: Hierarchy describe() and print() change
The changes motivated by two things:
- proc 0 is not aware of coarse level data, if it drops out early
- there is confusion which functions print local info, and which print
  global

Current status:
- Hierarchy::print() functions print local info, and take std::ostream
  as an input
- Hierarchy::describe() and Hierarchy::description() print out global
  info, and take FancyOStream as an input
- GetNumLevels() returns the number of levels in a local hierarchy, and
  GetGlobalNumLevels() returns the number of levels in the full
  hierarchy

I also removed unused GetNumberOfLevels(), which did exactly same thing
as GetNumLevels().

packages/muelu/src/MueCentral/MueLu_Level.cpp
@@ -354,20 +354,6 @@ namespace MueLu {
     return out.str();
   }
 
-  void Level::print(Teuchos::FancyOStream& out, const VerbLevel verbLevel) const {
-    RCP<Teuchos::FancyOStream> out0 = Teuchos::rcpFromRef(out);
-    int previousSetting = out0->getOutputToRootOnly();
-    out0->setShowProcRank(true);
-
-    std::ostringstream ss;
-    print(ss, verbLevel);
-
-    out0->setOutputToRootOnly(-1);
-    *out0 << ss.str() << std::endl;
-    out0->setOutputToRootOnly(previousSetting);
-    out0->setShowProcRank(false);
-  }
-
   void Level::print(std::ostream& out, const VerbLevel verbLevel) const {
     if (!(verbLevel & Debug))
       return;
@@ -473,7 +459,7 @@ namespace MueLu {
                             std::map<std::pair<BoostVertex, BoostVertex>, std::string>&  edges,
                             BoostProperties&                                             dp,
                             BoostGraph&                                                  graph) const {
-      size_t vind = vindices.size();
+      s)ize_t vind = vindices.size();
 
       for (TwoKeyMap::const_iterator it1 = map_.begin(); it1 != map_.end(); it1++) {
         if (vindices.find(it1->first) == vindices.end()) {

commit 58f6e01439ec17c6e1b76c91c2dfd722fae16941

MueLu: bugfix for typo preventing compilation (introduced in 6e712e60682a16d6aa)

packages/muelu/src/MueCentral/MueLu_Level.cpp
@@ -459,7 +459,7 @@ namespace MueLu {
                             std::map<std::pair<BoostVertex, BoostVertex>, std::string>&  edges,
                             BoostProperties&                                             dp,
                             BoostGraph&                                                  graph) const {
-      s)ize_t vind = vindices.size();
+      size_t vind = vindices.size();
 
       for (TwoKeyMap::const_iterator it1 = map_.begin(); it1 != map_.end(); it1++) {
         if (vindices.find(it1->first) == vindices.end()) {