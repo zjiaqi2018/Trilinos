zjiaqi2018/Trilinos

commit f001af94d4115a90f54f14a0341644c941c9be76

MiniTensor: Introduce capability to return a user-set error message when setting a failure flag in MiniSolver. The failure flag provides a mechanism to abort a function evaluation or solution method when an unrecoverable failure has been detected. This is used take other recovery actions that are beyond the scope of MiniSolver (e.g. In Albany use NOX to decrease the step size when setting the failure flag within the evaluation of a material model). (#1132)

packages/minitensor/src/MiniTensor_Solvers.h
@@ -63,6 +63,10 @@ template<typename FunctionDerived, typename S, Index M>
 struct Function_Base
 {
 public:
+  static constexpr
+  Index
+  DIMENSION{M};
+
   Function_Base()
   {
   }
@@ -96,15 +100,32 @@ public:
   Tensor<T, N>
   hessian(FunctionDerived & f, Vector<T, N> const & x);
 
+  void
+  set_failed(char const * const msg = nullptr);
+
+  bool
+  get_failed();
+
+  void
+  clear_failed();
+
+  void
+  set_failure_message(char const * const msg = nullptr);
+
+  char const * const
+  get_failure_message();
+
+private:
   ///
   /// Signal that something has gone horribly wrong.
   ///
   bool
   failed{false};
 
-  static constexpr
-  Index
-  DIMENSION{M};
+  ///
+  /// Keep a message to inform what went wrong above.
+  char const *
+  failure_message{nullptr};
 };
 
 ///
@@ -279,6 +300,9 @@ public:
 
   char const *
   function_name{nullptr};
+
+  char const *
+  failure_message{"No failure detected"};
 };
 
 ///

packages/minitensor/src/MiniTensor_FunctionSet.h
@@ -787,14 +787,8 @@ public:
   T
   value(Vector<T, N> const & X)
   {
-    // Get a convenience reference to the failed flag in case it is used more
-    // than once.
-    bool &
-    failed = Base::failed;
-
     // Set the flag to signal that an unrecoverable error happened.
-
-    failed = true;
+    this->set_failed("Testing failure mechanism");
 
     T const
     fn = 0.0;

packages/minitensor/src/MiniTensor_Solvers.t.h
@@ -148,6 +148,65 @@ hessian(FunctionDerived & f, Vector<T, N> const & x)
 //
 //
 //
+template<typename FunctionDerived, typename S, Index M>
+void
+Function_Base<FunctionDerived, S, M>::
+set_failed(char const * const msg)
+{
+  failed = true;
+  failure_message = msg;
+  return;
+}
+
+//
+//
+//
+template<typename FunctionDerived, typename S, Index M>
+bool
+Function_Base<FunctionDerived, S, M>::
+get_failed()
+{
+  return failed;
+}
+
+//
+//
+//
+template<typename FunctionDerived, typename S, Index M>
+void
+Function_Base<FunctionDerived, S, M>::
+clear_failed()
+{
+  failed = false;
+  return;
+}
+
+//
+//
+//
+template<typename FunctionDerived, typename S, Index M>
+void
+Function_Base<FunctionDerived, S, M>::
+set_failure_message(char const * const msg)
+{
+  failure_message = msg;
+  return;
+}
+
+//
+//
+//
+template<typename FunctionDerived, typename S, Index M>
+char const * const
+Function_Base<FunctionDerived, S, M>::
+get_failure_message()
+{
+  return failure_message;
+}
+
+//
+//
+//
 template<typename ConstraintDerived, typename S, Index NC, Index NV>
 template<typename T, Index N>
 Vector<T, NC>
@@ -242,7 +301,8 @@ solve(STEP & step_method, FN & fn, Vector<T, N> & soln)
 
   initial_value = fn.value(soln);
   previous_value = initial_value;
-  failed = failed || fn.failed;
+  failed = failed || fn.get_failed();
+  if (fn.get_failed() == true) failure_message = fn.get_failure_message();
   initial_norm = norm(resi);
 
   updateConvergenceCriterion(initial_norm);
@@ -258,7 +318,8 @@ solve(STEP & step_method, FN & fn, Vector<T, N> & soln)
 
     resi = fn.gradient(soln);
 
-    failed = failed || fn.failed;
+    failed = failed || fn.get_failed();
+    if (fn.get_failed() == true) failure_message = fn.get_failure_message();
 
     T const
     norm_resi = norm(resi);
@@ -268,7 +329,8 @@ solve(STEP & step_method, FN & fn, Vector<T, N> & soln)
     T const
     value = fn.value(soln);
 
-    failed = failed || fn.failed;
+    failed = failed || fn.get_failed();
+    if (fn.get_failed() == true) failure_message = fn.get_failure_message();
 
     updateDivergenceCriterion(value);
 
@@ -353,6 +415,7 @@ updateDivergenceCriterion(T const fn_value)
 
   if (enforce_monotonicity == true && monotonic == false) {
     failed = true;
+    failure_message = "Non-monotonic";
   }
 
   previous_value = fn_value;
@@ -361,6 +424,7 @@ updateDivergenceCriterion(T const fn_value)
 
   if (enforce_boundedness == true && bounded == false) {
     failed = true;
+    failure_message = "Growing unbounded";
   }
 
   return;

packages/minitensor/test/test_02.cc
@@ -580,6 +580,16 @@ TEST(MiniTensor, MixedStorage)
 
 TEST(MiniTensor, FailedFlag)
 {
+  bool const
+  print_output = ::testing::GTEST_FLAG(print_time);
+
+  // outputs nothing
+  Teuchos::oblackholestream
+  bhs;
+
+  std::ostream &
+  os = (print_output == true) ? std::cout : bhs;
+
   constexpr Index
   dimension{1};
 
@@ -603,11 +613,23 @@ TEST(MiniTensor, FailedFlag)
 
   minimizer.solve(step, fn, x);
 
+  os << minimizer.failure_message << '\n';
+
   ASSERT_EQ(minimizer.failed, true);
 }
 
 TEST(MiniTensor, Monotonicity)
 {
+  bool const
+  print_output = ::testing::GTEST_FLAG(print_time);
+
+  // outputs nothing
+  Teuchos::oblackholestream
+  bhs;
+
+  std::ostream &
+  os = (print_output == true) ? std::cout : bhs;
+
   constexpr Index
   dimension{1};
 
@@ -632,6 +654,8 @@ TEST(MiniTensor, Monotonicity)
   x(0) = 2.0;
 
   minimizer.solve(step, fn, x);
+
+  os << minimizer.failure_message << '\n';
 
   ASSERT_EQ(minimizer.monotonic, false);
   ASSERT_EQ(minimizer.failed, true);
@@ -673,6 +697,8 @@ TEST(MiniTensor, Boundedness)
   x(0) = 0.5;
 
   minimizer.solve(step, fn, x);
+
+  os << minimizer.failure_message << '\n';
 
   minimizer.printReport(os);
 

packages/rol/adapters/minitensor/src/sol/ROL_MiniTensor_MiniSolver.hpp
@@ -175,6 +175,9 @@ public:
 
   char const *
   function_name{nullptr};
+
+  char const *
+  failure_message{"No failure detected"};
 };
 
 } // namespace ROL

packages/rol/adapters/minitensor/src/sol/ROL_MiniTensor_MiniSolver_Def.hpp
@@ -88,7 +88,8 @@ solve(
 
   initial_value = fn.value(soln);
   previous_value = initial_value;
-  failed = failed || fn.failed;
+  failed = failed || fn.get_failed();
+  if (fn.get_failed() == true) failure_message = fn.get_failure_message();
   initial_norm = minitensor::norm(resi);
 
   // Define algorithm.

commit 58b0fd347ff708bf530946004a3b4fd528ab5976

MiniTensor: give protected access to bool failed
There was code in the Albany CrystalPlasticity
model that directly accesses this boolean, and
got broken by f001af9.
This commit fixes the build.

packages/minitensor/src/MiniTensor_Solvers.h
@@ -115,7 +115,7 @@ public:
   char const * const
   get_failure_message();
 
-private:
+protected:
   ///
   /// Signal that something has gone horribly wrong.
   ///