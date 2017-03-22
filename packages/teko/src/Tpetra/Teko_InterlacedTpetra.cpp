zjiaqi2018/Trilinos

commit e728fc01885c8007ba8e9af1f8e790e49d891e46

Teko: Removing some warnings

packages/teko/src/Tpetra/Teko_InterlacedTpetra.cpp
@@ -122,7 +122,6 @@ void buildSubMaps(GO numGlobals,LO numMyElements,GO minMyGID,const std::vector<i
    for(varItr=vars.begin();varItr!=vars.end();++varItr) {
       LO numLocalVars = *varItr;
       GO numAllElmts = numLocalVars*numGlobals/numGlobalVars;
-      LO numMyElmts = numLocalVars * numBlocks;
 
       // create global arrays describing the as of yet uncreated maps
       std::vector<GO> subGlobals;

packages/teko/tests/src/Epetra/tEpetraThyraConverter.cpp
@@ -90,33 +90,6 @@ double compareEpetraMVToThyra(const Epetra_MultiVector & eX,
                             const Teuchos::RCP<const Thyra::MultiVectorBase<double> > & tX,
                             int verbosity,std::ostream & os,int indexStart=-1,int indexEnd=-1);
 
-const RCP<const Thyra::VectorSpaceBase<double> > buildCompositeSpace(const Epetra_Comm & Comm)
-{
-   const RCP<const Teuchos::Comm<Teuchos::Ordinal> > tComm = Thyra::create_Comm(rcpFromRef(Comm));
-
-   // get process information
-   int numProc = Comm.NumProc();
-   // int myPID   = Comm.MyPID();
-
-   // how big is this vector
-   int uElmts = 4;
-   int vElmts = 8;
-   int pElmts = 6;
-
-   // build vector space
-   Teuchos::Array<RCP<const Thyra::VectorSpaceBase<double> > > uvArray;
-   uvArray.push_back(Thyra::defaultSpmdVectorSpace<double>(tComm,uElmts,uElmts*numProc)); 
-   uvArray.push_back(Thyra::defaultSpmdVectorSpace<double>(tComm,vElmts,vElmts*numProc)); 
-
-   Teuchos::Array<RCP<const Thyra::VectorSpaceBase<double> > > uvpArray;
-   uvpArray.push_back(Thyra::productVectorSpace<double>(uvArray));
-   uvpArray.push_back(Thyra::defaultSpmdVectorSpace<double>(tComm,pElmts,pElmts*numProc)); 
-
-   const RCP<const Thyra::VectorSpaceBase<double> > uvpVS = Thyra::productVectorSpace<double>(uvpArray);
-
-   return uvpVS;
-}
-
 double compareEpetraMVToThyra(const Epetra_MultiVector & eX,
                             const Teuchos::RCP<const Thyra::MultiVectorBase<double> > & tX,
                             int verbosity,std::ostream & os,int indexStart,int indexEnd)

packages/teko/tests/src/Tpetra/tTpetraThyraConverter.cpp
@@ -89,33 +89,6 @@ double compareTpetraMVToThyra(const Tpetra::MultiVector<ST,LO,GO,NT> & eX,
                             const Teuchos::RCP<const Thyra::MultiVectorBase<ST> > & tX,
                             int verbosity,std::ostream & os,GO indexStart=-1,GO indexEnd=-1);
 
-const RCP<const Thyra::VectorSpaceBase<ST> > buildCompositeSpace(const Teuchos::Comm<int> & Comm)
-{
-   const RCP<const Teuchos::Comm<Teuchos::Ordinal> > tComm = Thyra::convertTpetraToThyraComm(rcpFromRef(Comm));
-
-   // get process information
-   int numProc = Comm.getSize();
-   // int myPID   = Comm.MyPID();
-
-   // how big is this vector
-   GO uElmts = 4;
-   GO vElmts = 8;
-   GO pElmts = 6;
-
-   // build vector space
-   Teuchos::Array<RCP<const Thyra::VectorSpaceBase<ST> > > uvArray;
-   uvArray.push_back(Thyra::defaultSpmdVectorSpace<ST>(tComm,uElmts,uElmts*numProc)); 
-   uvArray.push_back(Thyra::defaultSpmdVectorSpace<ST>(tComm,vElmts,vElmts*numProc)); 
-
-   Teuchos::Array<RCP<const Thyra::VectorSpaceBase<ST> > > uvpArray;
-   uvpArray.push_back(Thyra::productVectorSpace<ST>(uvArray));
-   uvpArray.push_back(Thyra::defaultSpmdVectorSpace<ST>(tComm,pElmts,pElmts*numProc)); 
-
-   const RCP<const Thyra::VectorSpaceBase<ST> > uvpVS = Thyra::productVectorSpace<ST>(uvpArray);
-
-   return uvpVS;
-}
-
 double compareTpetraMVToThyra(const Tpetra::MultiVector<ST,LO,GO,NT> & eX,
                             const Teuchos::RCP<const Thyra::MultiVectorBase<ST> > & tX,
                             int verbosity,std::ostream & os,GO indexStart,GO indexEnd)

commit b05795c29d013f522bc2a95adcc7e60d85fd0337

Teko: fix compile error introduced by e728fc0
this variable does need to be defined if
the assert() call will be using it, so
it needs to be protected by the same
condition that controls assert(),
which is NDEBUG

packages/teko/src/Tpetra/Teko_InterlacedTpetra.cpp
@@ -122,6 +122,9 @@ void buildSubMaps(GO numGlobals,LO numMyElements,GO minMyGID,const std::vector<i
    for(varItr=vars.begin();varItr!=vars.end();++varItr) {
       LO numLocalVars = *varItr;
       GO numAllElmts = numLocalVars*numGlobals/numGlobalVars;
+#ifndef NDEBUG
+      LO numMyElmts = numLocalVars * numBlocks;
+#endif
 
       // create global arrays describing the as of yet uncreated maps
       std::vector<GO> subGlobals;