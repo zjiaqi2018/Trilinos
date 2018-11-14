zjiaqi2018/Trilinos

commit 36351ef00d8b9272d643b74ae62e1ad1d25f9f9b

Intrepid2: add overloads of functions DefaultCubatureFactory::create(...) and OrientationTools::mapToModifiedReference(...)            that accept as input directly the cell topology key (that uniquely identify a topology) rather than the topology object itself.

packages/intrepid2/src/Discretization/Integration/Intrepid2_DefaultCubatureFactoryDef.hpp
@@ -55,14 +55,14 @@ namespace Intrepid2 {
   template<typename SpT, typename PT, typename WT>
   Teuchos::RCP<Cubature<SpT,PT,WT> > 
   DefaultCubatureFactory::
-  create( const shards::CellTopology       cellTopology,
+  create( unsigned topologyKey,
           const std::vector<ordinal_type> &degree,
           const EPolyType                  polytype ) {
 
     // Create generic cubature.
     Teuchos::RCP<Cubature<SpT,PT,WT> > r_val;
 
-    switch (cellTopology.getBaseCellTopologyData()->key) {
+    switch (topologyKey) {
     case shards::Line<>::key: {
       INTREPID2_TEST_FOR_EXCEPTION( degree.size() < 1, std::invalid_argument,
                                     ">>> ERROR (DefaultCubatureFactory): Provided degree array is of insufficient length.");
@@ -142,14 +142,14 @@ namespace Intrepid2 {
       break;
     }
     default: {
-      INTREPID2_TEST_FOR_EXCEPTION( ( (cellTopology.getBaseCellTopologyData()->key != shards::Line<>::key)             &&
-                                      (cellTopology.getBaseCellTopologyData()->key != shards::Triangle<>::key)         &&
-                                      (cellTopology.getBaseCellTopologyData()->key != shards::Quadrilateral<>::key)    &&
-                                      (cellTopology.getBaseCellTopologyData()->key != shards::ShellQuadrilateral<>::key)    &&
-                                      (cellTopology.getBaseCellTopologyData()->key != shards::Tetrahedron<>::key)      &&
-                                      (cellTopology.getBaseCellTopologyData()->key != shards::Hexahedron<>::key)       &&
-                                      (cellTopology.getBaseCellTopologyData()->key != shards::Pyramid<>::key)          &&
-                                      (cellTopology.getBaseCellTopologyData()->key != shards::Wedge<>::key) ),
+      INTREPID2_TEST_FOR_EXCEPTION( ( (topologyKey != shards::Line<>::key)               &&
+                                      (topologyKey != shards::Triangle<>::key)           &&
+                                      (topologyKey != shards::Quadrilateral<>::key)      &&
+                                      (topologyKey != shards::ShellQuadrilateral<>::key) &&
+                                      (topologyKey != shards::Tetrahedron<>::key)        &&
+                                      (topologyKey != shards::Hexahedron<>::key)         &&
+                                      (topologyKey != shards::Pyramid<>::key)            &&
+                                      (topologyKey != shards::Wedge<>::key) ),
                                     std::invalid_argument,
                                     ">>> ERROR (DefaultCubatureFactory): Invalid cell topology prevents cubature creation.");
     }
@@ -157,16 +157,36 @@ namespace Intrepid2 {
     return r_val;
   }
 
+  template<typename SpT, typename PT, typename WT>
+  Teuchos::RCP<Cubature<SpT,PT,WT> >
+  DefaultCubatureFactory::
+  create( const shards::CellTopology       cellTopology,
+          const std::vector<ordinal_type> &degree,
+          const EPolyType                  polytype) {
+       return create<SpT,PT,WT>(cellTopology.getKey(), degree, polytype);
+  }
+
 
   template<typename SpT, typename PT, typename WT>
   Teuchos::RCP<Cubature<SpT,PT,WT> > 
+  DefaultCubatureFactory::
+  create( unsigned topologyKey,
+          const ordinal_type         degree,
+          const EPolyType            polytype ) {
+    // uniform order for 3 axes
+    const std::vector<ordinal_type> degreeArray(3, degree);
+    return create<SpT,PT,WT>(topologyKey, degreeArray, polytype);
+  }
+
+  template<typename SpT, typename PT, typename WT>
+  Teuchos::RCP<Cubature<SpT,PT,WT> >
   DefaultCubatureFactory::
   create( const shards::CellTopology cellTopology,
           const ordinal_type         degree,
           const EPolyType            polytype ) {
     // uniform order for 3 axes
     const std::vector<ordinal_type> degreeArray(3, degree);
-    return create<SpT,PT,WT>(cellTopology, degreeArray, polytype);
+    return create<SpT,PT,WT>(cellTopology.getKey(), degreeArray, polytype);
   }
 
 

packages/intrepid2/src/Discretization/Integration/Intrepid2_DefaultCubatureFactory.hpp
@@ -78,7 +78,7 @@ namespace Intrepid2 {
 
     /** \brief Factory method.
 
-        \param cell        [in]    - Cell topology.
+        \param topologyKey [in]    - Key of the cell topology.
         \param degree      [in]    - Array of polynomial degrees, one for each component cubature.
 
         \return
@@ -88,10 +88,42 @@ namespace Intrepid2 {
              typename pointValueType = double,
              typename weightValueType = double>
     static Teuchos::RCP<Cubature<ExecSpaceType,pointValueType,weightValueType> > 
+    create( unsigned                         topologyKey,
+            const std::vector<ordinal_type> &degree,
+            const EPolyType                  polytype = POLYTYPE_MAX );
+
+    /** \brief Factory method.
+
+        \param cell        [in]    - Cell topology.
+        \param degree      [in]    - Array of polynomial degrees, one for each component cubature.
+
+        \return
+        - RCP to cubature with given specifications.
+    */
+    template<typename ExecSpaceType,
+             typename pointValueType = double,
+             typename weightValueType = double>
+    static Teuchos::RCP<Cubature<ExecSpaceType,pointValueType,weightValueType> >
     create( const shards::CellTopology       cellTopology,
             const std::vector<ordinal_type> &degree,
             const EPolyType                  polytype = POLYTYPE_MAX );
 
+
+    /** \brief Factory method.
+
+        \param topologyKey [in]    - Key of the cell topology.
+        \param degree      [in]    - A single polynomial degree, used for all component cubatures.
+
+        \return
+        - RCP to cubature with given specifications.
+    */
+    template<typename ExecSpaceType,
+             typename pointValueType = double,
+             typename weightValueType = double>
+    static Teuchos::RCP<Cubature<ExecSpaceType,pointValueType,weightValueType> >
+    create( unsigned                    topologyKey,
+            const ordinal_type          degree,
+            const EPolyType             polytype = POLYTYPE_MAX );
     
     /** \brief Factory method.
 

packages/intrepid2/src/Orientation/Intrepid2_OrientationTools.hpp
@@ -179,6 +179,22 @@ namespace Intrepid2 {
                              const shards::CellTopology cellTopo,
                              const ordinal_type cellOrt = 0);
 
+      /** \brief  Computes modified parameterization maps of 1- and 2-subcells with orientation.
+
+          \param  outPoints       [out] - rank-2 (P,D2) array with points in 1D or 2D modified domain with orientation
+          \param  refPoints       [in]  - rank-2 (P,D2) array with points in 1D or 2D parameter domain
+          \param  cellTopoKey     [in]  - key of the cell topology of the parameterized domain (1- and 2-subcells)
+          \param  cellOrt         [in]  - cell orientation number (zero is aligned with shards default configuration
+      */
+      template<typename outPointViewType,
+               typename refPointViewType>
+      inline
+      static void
+      mapToModifiedReference(outPointViewType outPoints,
+                             const refPointViewType refPoints,
+                             const unsigned cellTopoKey,
+                             const ordinal_type cellOrt = 0);
+
 
       /** \brief  Computes Jacobian of orientation map for line segment.
 
@@ -222,6 +238,19 @@ namespace Intrepid2 {
       static void
       getJacobianOfOrientationMap(JacobianViewType jacobian,
                              const shards::CellTopology cellTopo,
+                             const ordinal_type cellOrt);
+
+      /** \brief  Computes jacobian of the parameterization maps of 1- and 2-subcells with orientation.
+
+          \param  jacobian        [out] - rank-2 (D,D) array with jacobian of the orientation map
+          \param  cellTopoKey     [in]  - key of the cell topology of the parameterized domain (1- and 2-subcells)
+          \param  cellOrt         [in]  - cell orientation number (zero is aligned with shards default configuration
+      */
+      template<typename JacobianViewType>
+      inline
+      static void
+      getJacobianOfOrientationMap(JacobianViewType jacobian,
+                             const unsigned cellTopoKey,
                              const ordinal_type cellOrt);
 
       // -----------------------------------------------------------------------------
@@ -438,7 +467,30 @@ namespace Intrepid2 {
     modifyBasisByOrientation(      Kokkos::DynRankView<outputValueType,outputProperties...> output,
                              const Kokkos::DynRankView<inputValueType, inputProperties...>  input,
                              const Kokkos::DynRankView<ortValueType,   ortProperties...> orts,
-                             const BasisPtrType basis);
+                             const BasisPtrType  basis);
+
+    template<typename ExecutionSpaceType, typename dofCoordsValueType,
+             typename dofCoeffsValueType, typename BasisType>
+    inline
+    static void
+    getSubCellBases(/**/
+      Teuchos::RCP<Basis<ExecutionSpaceType, dofCoeffsValueType,dofCoordsValueType> >* faceBases,
+      Teuchos::RCP<Basis<ExecutionSpaceType, dofCoeffsValueType,dofCoordsValueType> >& edgeBasis,
+      const BasisType&  basis,
+      EPointType pointType = POINTTYPE_EQUISPACED);
+
+    template<typename dofCoordsValueType, class ...dofCoordsProperties,
+             typename dofCoeffsValueType, class ...dofCoeffsProperties,
+             typename ortValueType,       class ...ortProperties,
+             typename BasisType>
+    inline
+    static void
+    getModifiedDofs(/**/  Kokkos::DynRankView<dofCoordsValueType,dofCoordsProperties...> orientedDofCoords,
+      Kokkos::DynRankView<dofCoeffsValueType,dofCoeffsProperties...> orientedDofCoeffs,
+      const Kokkos::DynRankView<ortValueType,   ortProperties...>    orts,
+      const BasisType& basis,
+      EPointType pointType = POINTTYPE_EQUISPACED);
+
   };
   
   template<typename T> 
@@ -452,7 +504,6 @@ namespace Intrepid2 {
 #include "Intrepid2_OrientationToolsDefCoeffMatrix_HCURL.hpp"
 #include "Intrepid2_OrientationToolsDefCoeffMatrix_HDIV.hpp"
 #include "Intrepid2_OrientationToolsDefMatrixData.hpp"
-
 #include "Intrepid2_OrientationToolsDefModifyBasis.hpp"
 
 #endif

packages/intrepid2/src/Orientation/Intrepid2_OrientationToolsDefModifyPoints.hpp
@@ -114,15 +114,16 @@ namespace Intrepid2 {
                              pt1 };
       
 #ifdef HAVE_INTREPID2_DEBUG
-      INTREPID2_TEST_FOR_ABORT( !( 0.0 <= lambda[0] && lambda[0] <= 1.0 ), 
+      VT eps = 10.0*std::numeric_limits<VT>::epsilon();
+      INTREPID2_TEST_FOR_ABORT( !( -eps <= lambda[0] && lambda[0] <= 1.0+eps ),
                                 ">>> ERROR (Intrepid::OrientationTools::getModifiedTrianglePoint): " \
                                 "Computed bicentric coordinate (lamba[0]) is out of range [0, 1].");
       
-      INTREPID2_TEST_FOR_ABORT( !( 0.0 <= lambda[1] && lambda[1] <= 1.0 ), 
+      INTREPID2_TEST_FOR_ABORT( !( -eps <= lambda[1] && lambda[1] <= 1.0+eps ),
                                 ">>> ERROR (Intrepid::OrientationTools::getModifiedTrianglePoint): " \
                                 "Computed bicentric coordinate (lamba[1]) is out of range [0, 1].");
       
-      INTREPID2_TEST_FOR_ABORT( !( 0.0 <= lambda[2] && lambda[2] <= 1.0 ), 
+      INTREPID2_TEST_FOR_ABORT( !( -eps <= lambda[2] && lambda[2] <= 1.0+eps ),
                                 ">>> ERROR (Intrepid::OrientationTools::getModifiedTrianglePoint): "
                                 "Computed bicentric coordinate (lamba[2]) is out of range [0, 1].");
 #endif
@@ -267,10 +268,22 @@ namespace Intrepid2 {
       }
 #endif
       
+      return mapToModifiedReference(outPoints, refPoints, cellTopo.getKey(), cellOrt);
+    }
+
+
+    template<typename outPointViewType,
+             typename refPointViewType>
+    inline
+    void
+    OrientationTools::
+    mapToModifiedReference(outPointViewType outPoints,
+                           const refPointViewType refPoints,
+                           const unsigned cellTopoKey,
+                           const ordinal_type cellOrt) {
       // Apply the parametrization map to every point in parameter domain
       const ordinal_type numPts = outPoints.extent(0);
-      const auto key = cellTopo.getBaseCellTopologyData()->key;
-      switch (key) {
+      switch (cellTopoKey) {
       case shards::Line<>::key : {
         for (ordinal_type pt=0;pt<numPts;++pt)
           getModifiedLinePoint(outPoints(pt, 0),
@@ -293,9 +306,9 @@ namespace Intrepid2 {
         break;
       }
       default: {
-        INTREPID2_TEST_FOR_WARNING( true, 
+        INTREPID2_TEST_FOR_ABORT( true,
                                     ">>> ERROR (Intrepid2::OrientationTools::mapToModifiedReference): " \
-                                    "Invalid cell topology." );
+                                    "Invalid cell topology key." );
         break;
       }
       }
@@ -326,8 +339,17 @@ namespace Intrepid2 {
       }
 #endif
 
-      const auto key = cellTopo.getBaseCellTopologyData()->key;
-      switch (key) {
+      return getJacobianOfOrientationMap(jacobian, cellTopo.getKey(), cellOrt);
+    }
+
+    template<typename outPointViewType>
+    inline
+    void
+    OrientationTools::
+    getJacobianOfOrientationMap(outPointViewType jacobian,
+                           const unsigned cellTopoKey,
+                           const ordinal_type cellOrt) {
+      switch (cellTopoKey) {
       case shards::Line<>::key :
         getLineJacobian(jacobian, cellOrt);
         break;
@@ -338,9 +360,9 @@ namespace Intrepid2 {
           getQuadrilateralJacobian(jacobian, cellOrt);
         break;
       default: {
-        INTREPID2_TEST_FOR_WARNING( true,
+        INTREPID2_TEST_FOR_ABORT( true,
                                     ">>> ERROR (Intrepid2::OrientationTools::mapToModifiedReference): " \
-                                    "Invalid cell topology." );
+                                    "Invalid cell topology key." );
         break;
       }
       }

commit 3c5f985f9678434aae1affefc261b44e0feb8419

Intrepid2: fixing bug in DefaultCubatureFactory introduced in commit 36351ef            (using the cell topology instead of the cell base topology)

packages/intrepid2/src/Discretization/Integration/Intrepid2_DefaultCubatureFactoryDef.hpp
@@ -163,7 +163,7 @@ namespace Intrepid2 {
   create( const shards::CellTopology       cellTopology,
           const std::vector<ordinal_type> &degree,
           const EPolyType                  polytype) {
-       return create<SpT,PT,WT>(cellTopology.getKey(), degree, polytype);
+       return create<SpT,PT,WT>(cellTopology.getBaseKey(), degree, polytype);
   }
 
 
@@ -186,7 +186,7 @@ namespace Intrepid2 {
           const EPolyType            polytype ) {
     // uniform order for 3 axes
     const std::vector<ordinal_type> degreeArray(3, degree);
-    return create<SpT,PT,WT>(cellTopology.getKey(), degreeArray, polytype);
+    return create<SpT,PT,WT>(cellTopology.getBaseKey(), degreeArray, polytype);
   }
 
 