f134b9226b2a68fa680cfd440368c33679955d62
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-07-21 16:03:41 -0600

    ATDM: ats1: Disable hsw_intel-18_opt build to debug 'git fetch' error (ATDV-377)

--- cmake/std/atdm/ats1/all_supported_builds.sh
+++ cmake/std/atdm/ats1/all_supported_builds.sh
@@ -9,5 +9,7 @@ export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=(
   ats1-knl_intel-19.0.4_mpich-7.7.6_openmp_static_dbg
   ats1-knl_intel-18.0.5_mpich-7.7.6_openmp_static_dbg
   ats1-hsw_intel-19.0.4_mpich-7.7.6_openmp_static_dbg
-  ats1-hsw_intel-18.0.5_mpich-7.7.6_openmp_static_dbg
+  #ats1-hsw_intel-18.0.5_mpich-7.7.6_openmp_static_dbg
+  # NOTE: Commented out last build to try to debut a git fetch problem on
+  # 'mutrino' (see ATDV-377)
   )
